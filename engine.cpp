dcde80cbb5c1798f1ab7be3eec392754ad95c50b
vpobol <vpobol@startyourtime.com>
2025-04-25 21:29:58 +0300

    add funcs in tensor

--- engine.cpp
+++ engine.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <Eigen/Dense>
 #include <unsupported/Eigen/CXX11/Tensor>
+#include <random>
+#include <functional>
 
 
 using namespace std;
@@ -8,11 +10,40 @@ using namespace Eigen;
 
 class Tensor{
 public:
-    Matrix <double, 3,3> value;
-    void zeros(int a, int b, int c, int d){
-        value = 
+
+    Eigen::Tensor<double, 4> tensor; /* создаем экземпляр класса тензор*/
+    Eigen::Tensor<double, 4> grad;
+    function<void()> _backward;
+
+    Tensor(int a, int b, int c, int d): tensor(a,b,c,d), grad(a,b,c,d){
+        tensor.setZero();
+        grad.setZero();
     }
-}
+
+    void random(int a, int b, int c, int d){
+        random_device rd;
+        mt19937 gen(rd());
+        normal_distribution<float> dist(0.0, 1.0);
+
+        for(int i = 0; i < tensor.size(); ++i){
+            tensor.data()[i] = dist(gen);
+        }
+    }
+
+    Tensor operator+(const Tensor& other) const{
+        Tensor result(tensor.dimension(0), tensor.dimension(1), tensor.dimension(2), tensor.dimension(3));
+        result.tensor = tensor + other.tensor;
+        result.grad.setZero();
+
+        result._backward = [this, other, &result]() mutable{
+            this->grad += result.grad;
+            other.grad += result.grad;
+        };
+
+        return result;
+    }
+
+};
 
 int main(){
     Matrix <float, 3,3,3,3> matrix_A;
